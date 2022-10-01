158bef0b516599dec6181b8d8cbff5876fb43cb7
jadefred <97032726+jadefred@users.noreply.github.com>
2022-10-01 17:08:54 +0200

    draw bar chart container

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(d3)
+const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+const width = 800;
+const height = 400;
+const margin = 100;
+let dataArr;
+
+let svg = d3.select("#svgContainer");
+
+const drawContainer = () => {
+  svg.append('svg').attr("height", height + margin).attr("width", width + margin);
+};
+
+async function drawBarChart() {
+  const response = await fetch(url);
+  const data = await response.json();
+  drawContainer();
+}
+
+drawBarChart();
